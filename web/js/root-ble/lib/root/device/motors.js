b4ae30f47a9a518734e2a7f6abd1dd7bf1156bc3
Danilo Caporale <danilo.caporale@gmail.com>
2020-04-23 18:01:54 +0200

    First commit. Workin version.

---
+++ web/js/root-ble/lib/root/device/motors.js
@@ -0,0 +1,111 @@
+function RootDeviceMotors(root) {
+  this.init(root);
+}
+
+Object.assign(RootDeviceMotors.prototype, {
+  init: function(root) {
+    this.root = root;
+    this.device = 1;
+
+    this.STALL_MOTOR_LEFT = 0;
+    this.STALL_MOTOR_RIGHT = 1;
+    this.STALL_MOTOR_MARKER_ERASER = 2;
+
+    this.STALL_CAUSE_NO_STALL = 0;
+    this.STALL_CAUSE_OVERCURRENT = 1;
+    this.STALL_CAUSE_UNDERCURRENT = 2;
+    this.STALL_CAUSE_UNDERSPEED = 3;
+    this.STALL_CAUSE_SATURATED_PID = 4;
+    this.STALL_CAUSE_TIMEOUT = 5;
+
+    this.root.listenForRobotEvent(this.device, 29, this.motorStall.bind(this));
+  },
+
+  // leftSpeed (int32_t) 4 Bytes, Left motor speed in units of mm/s.
+  // Positive values are forwards, negative values are backwards.
+  // Minimum value of -100 0xFFFFFF9C. Maximum value of 100 0x00000064.
+  //
+  // rightSpeed (int32_t) 4 Bytes, Right motor speed in units of mm/s.
+  // Positive values are forwards, negative values are backwards.
+  // Minimum value of -100 0xFFFFFF9C. Maximum value of 100 0x00000064.
+  setLeftAndRightMotorSpeed: function (leftSpeed, rightSpeed) {
+    var command = 4;
+    var dataView = new DataView((new Uint8Array(8)).buffer);
+    dataView.setInt32(0, leftSpeed, false);
+    dataView.setInt32(4, rightSpeed, false);
+    var payload = new Uint8Array(dataView.buffer);
+    this.root.toRobot(this.device, command, payload);
+  },
+
+  setLeftMotorSpeed: function (leftSpeed) {
+    var command = 6;
+    var dataView = new DataView((new Uint8Array(4)).buffer);
+    dataView.setInt32(0, leftSpeed, false);
+    var payload = new Uint8Array(dataView.buffer);
+    this.root.toRobot(this.device, command, payload);
+  },
+
+  setRightMotorSpeed: function (rightSpeed) {
+    var command = 7;
+    var dataView = new DataView((new Uint8Array(4)).buffer);
+    dataView.setInt32(0, rightSpeed, false);
+    var payload = new Uint8Array(dataView.buffer);
+    this.root.toRobot(this.device, command, payload);
+  },
+
+  // driveDistance: function (distance, callback, my_robot) {
+  driveDistance: function (distance, callback) {
+    // document.getElementById('log').innerHTML += '<br>Inside driveDistance!';
+    var command = 8;
+    var dataView = new DataView((new Uint8Array(4)).buffer);
+    dataView.setInt32(0, distance, false);
+    var payload = new Uint8Array(dataView.buffer);
+    // document.getElementById('log').innerHTML += '<br>Sending message .toRobot';
+    // console.log(`my_robot is ${my_robot}`);
+    console.log(`this.root is ${this.root}`);
+    // my_robot.toRobot(this.device, command, payload, this.driveDistanceFinishedResponse(callback), null); // no timeout
+    this.root.toRobot(this.device, command, payload, this.driveDistanceFinishedResponse(callback), null); // no timeout
+  },
+  driveDistanceFinishedResponse: function (callback) {
+    // document.getElementById('log').innerHTML += '<br>Inside driveDistanceFinishedResponse!';
+    return callback;
+  },
+
+  rotateAngel: function (angle, callback) {
+    var command = 12;
+    var dataView = new DataView((new Uint8Array(4)).buffer);
+    dataView.setInt32(0, angle, false);
+    var payload = new Uint8Array(dataView.buffer);
+    this.root.toRobot(this.device, command, payload, this.rotateAngelFinishedResponse(callback), null); // no timeout
+  },
+  rotateAngelFinishedResponse: function (callback) {
+    return callback;
+  },
+
+  motorStall: function (payload) {
+    var timestamp = payload.getUint32(0);
+    var motor = payload.getUint8(4);
+    var cause = payload.getUint8(5);
+    this.dispatchEvent({
+      type: 'motorStall',
+      motor: motor,
+      cause: cause
+    });
+  },
+
+  dockRobot: function (callback) {
+    var command = 14;
+    var dataView = new DataView((new Uint8Array(4)).buffer);
+    dataView.setInt32(0, 0, false);
+    var payload = new Uint8Array(dataView.buffer);
+    this.root.toRobot(this.device, command, payload, this.dockRobotFinishedCallback(callback), null);
+  },
+  dockRobotFinishedCallback: function (callback)
+  {
+    console.log('Finished docking!');
+    return callback;
+  }
+
+});
+
+Object.assign(RootDeviceMotors.prototype, EventDispatcher.prototype);
---
+++ web/js/root-ble/main.js
@@ -0,0 +1,104 @@
+
+// const moveButton = document.getElementById('moveBtn');
+
+// moveButton.onclick = move;
+
+function move() {
+  console.log("Move button clicked");
+  // window.root.device.motors.setLeftAndRightMotorSpeed(50,50); // EPPUR SI MUOVE!
+  window.root.device.motors.driveDistance(100,fatto); // DA TESTARE
+}
+
+
+
+window.LOG = 1;
+
+var bleDevice = new BleDevice(Root.identifier, Root.services);
+
+var my_robot = "";
+
+bleDevice.addEventListener('connected', function (event) {
+  /*btnDisconnect.removeAttribute('disabled');
+  btnScanAndConnect.setAttribute('disabled', 'disabled');*/
+
+  var root = new Root(bleDevice);
+  root.setup(rootIsSetup);
+  // my_robot = root;
+  console.log('Robot connected OK');
+});
+
+bleDevice.addEventListener('disconnected', function (event) {
+  /*btnScanAndConnect.removeAttribute('disabled');
+  btnDisconnect.setAttribute('disabled', 'disabled');*/
+});
+
+/*var btnScanAndConnect = document.getElementById('btnScanAndConnect')
+var btnDisconnect = document.getElementById('btnDisconnect')
+
+btnScanAndConnect.addEventListener('pointerup', scanAndConnect)
+btnDisconnect.addEventListener('pointerup', disconnect)*/
+
+function switch_function_scan_conn(el_scan) {    
+    if (el_scan.checked) {
+      bleDevice.scanAndConnect();
+    }
+    else
+    {
+      bleDevice.disconnect();
+    } 
+  }
+
+/*function scanAndConnect (event) {
+  bleDevice.scanAndConnect();
+}
+
+function disconnect (event) {
+  bleDevice.disconnect();
+}*/
+
+function rootIsSetup (root) {
+  console.log('start using root', root);
+  window.root = root;
+
+  // moveButton.disabled = false;
+  // draw square example
+  document.getElementById('log').innerHTML += '<br>Connected!';
+
+  console.log('Muovo 15 cm');
+  // window.root.device.motors.driveDistance(150,fatto);
+  // root.device.motors.setLeftAndRightMotorSpeed(50,50);
+  console.log('Fine 15 cm');
+  // runQueue([go15cm, turn90deg, go15cm, turn90deg, go15cm, turn90deg, go15cm, turn90deg]);
+}
+
+function fatto()
+{
+  console.log('OK!')
+}
+
+// draw square example
+function runQueue(arr) {
+  document.getElementById('log').innerHTML += '<br>Starting motion command queue!';
+  var fnc = arr.shift();
+  var next = function () { runQueue(arr); };
+  if (arr.length<=0) {
+    next = function () { console.log('done'); }
+  }
+  fnc(next);
+}
+
+var go15cm = function (next) {
+  console.log('Begin of go15cm');
+  document.getElementById('log').innerHTML += '<br>Trying 15 cm forward!';
+  window.root.device.motors.driveDistance(150, next);
+  // window.root.device.motors.driveDistance(150, next, my_robot);
+  document.getElementById('log').innerHTML += '<br>Should have moved 15 cm forward!';
+  console.log('End of go15cm');
+}
+var turn90deg = function (next) {
+  console.log('Begin of turn90deg');
+  document.getElementById('log').innerHTML += '<br>Tring 90 deg rotation!';
+  window.root.device.motors.rotateAngel(900, next);
+  console.log('End of turn90deg');
+}
+
